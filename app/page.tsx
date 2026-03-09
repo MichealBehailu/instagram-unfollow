f09f17d4f13cdc45000760ca91abaa64da8602fa
Micheal <michealbehailu94@gmail.com>
2026-03-09 22:43:07 +0300

    issue getting full data

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import handler from "@/utils/handler";
+import notfollower from "@/lib/data/notfollower.json";
 
 export default function Home() {
-  console.log(handler());
-  return <div>Home</div>;
+  // console.log(handler());
+  
+  return <div>Homee</div>;
 }
--- utils/handler.ts
+++ utils/handler.ts
@@ -1,41 +1,31 @@
 import followers from "@/lib/data/followers.json";
 import following from "@/lib/data/following.json";
-// import notfollower from "@/lib/data/notfollower.json";
+import notfollower from "@/lib/data/notfollower.json";
 import fs from "fs";
 import path from "path";
-
-//create an array of object then finally push or write  that array on the json file
-
-
-const notfollowers:{title:string , link:string }[] =  [] //this array will store the not followers
-
-
+ 
 export default function handler(){
-
-  const followersLength = followers.length; //the number of my followers
-
-//   return followersLength;
-
-   for (let index = 0; index < followersLength; index++) {
-    
-    const followerUser = followers[index].string_list_data[0].value;
-    const followingUser = following.relationships_following[index].title;
-    
-    if(followerUser !== followingUser){
+  // Create a Set of follower usernames for O(1) lookup
+  const followerUsernames = new Set(
+    followers.map((f: any) => f.string_list_data[0]?.value)
+  );
+ 
+  const notfollowers: {title: string, link: string}[] = [];
+ 
+  // Iterate through all people you're following
+  for (const person of following.relationships_following) {
+    const username = person.title;
+ 
+    // If this person doesn't follow you back, add to list
+    if (!followerUsernames.has(username)) {
       notfollowers.push({
-        title: followingUser,
-        link: following.relationships_following[index].string_list_data[0].href
+        title: username,
+        link: person.string_list_data[0]?.href || ''
       });
     }
-    
-    
-   }
+  }
+  
+  //   Update the JSON file with the new data
+  const updatedData = JSON.stringify([...notfollower, ...notfollowers], null, 2);
+  fs.writeFileSync(path.join(process.cwd(), '/lib/data/notfollower.json'), updatedData, 'utf8');  
 }
-
-//    followers.map((follower) => {
-//     console.log(follower);
-//   });
-
-//   Update the JSON file with the new data
-//   const updatedData = JSON.stringify([...notfollower, followers[0]], null, 2);
-//   fs.writeFileSync(path.join(process.cwd(), '/lib/data/notfollower.json'), updatedData, 'utf8');  
