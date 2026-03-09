6abbd2af33a9af6b2a07c20fa04cafe882023284
Micheal <michealbehailu94@gmail.com>
2026-03-09 16:22:08 +0300

    creating handler file

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-
+import handler from "@/utils/handler";
 
 export default function Home() {
-
+  console.log(handler());
   return <div>Home</div>;
 }
--- utils/handler.ts
+++ utils/handler.ts
@@ -1,14 +1,31 @@
 import followers from "@/lib/data/followers.json";
-import notfollower from "@/lib/data/notfollower.json";
+import following from "@/lib/data/following.json";
+// import notfollower from "@/lib/data/notfollower.json";
 import fs from "fs";
 import path from "path";
 
-export default function handler(){
-   // followers.map((follower) => {
-  //   console.log(follower);
-  // });
+//create an array of object then finally push or write  that array on the json file
 
-  // Update the JSON file with the new data
-  // const updatedData = JSON.stringify([...notfollower, followers[0]], null, 2);
-  // fs.writeFileSync(path.join(process.cwd(), '/lib/data/notfollower.json'), updatedData, 'utf8');   
+
+const notfollowers:{title:string , link:string }[] =  [] //this array will store the not followers
+
+
+export default function handler(){
+
+    const followingLength = following.relationships_following.length; //the number of my following
+
+   for (let index = 0; index < followingLength; index++) {
+    
+    const element = following.relationships_following[index];
+    
+    
+   }
 }
+
+//    followers.map((follower) => {
+//     console.log(follower);
+//   });
+
+//   Update the JSON file with the new data
+//   const updatedData = JSON.stringify([...notfollower, followers[0]], null, 2);
+//   fs.writeFileSync(path.join(process.cwd(), '/lib/data/notfollower.json'), updatedData, 'utf8');  
